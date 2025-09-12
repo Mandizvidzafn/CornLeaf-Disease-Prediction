75c2ffd3439f3039c3739eec24b338ddbd4c3822
Nceba Mandizvidza <ncebamandizvidza@gmail.com>
2025-09-12 11:38:25 +0200

    Fixed the model predict function and added recommendation and about routes and pages

--- app.py
+++ app.py
@@ -4,34 +4,39 @@ import numpy as np
 from skimage import io
 from keras.utils import load_img, img_to_array
 from keras.preprocessing import image
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.applications import ResNet50
+from keras.models import load_model
+from flask import Flask, redirect, session, url_for, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
+app.secret_key = "corn_disease_prediction"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Initialize the database
+db = SQLAlchemy(app)
 
-#model =tf.keras.models.load_model('PlantDNet.h5',compile=False)
+# Load the model
+model = load_model('model/plant_disease_model.h5')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(img_path, model):
-    img = load_img(img_path, color_mode='rgb', target_size=(64, 64))
-    show_img = load_img(img_path, color_mode='rgb', target_size=(64, 64))
-    x = img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = np.array(x, 'float32')
-    x /= 255
-    preds = model.predict(x)
+    img = load_img(img_path, target_size=(224, 224))
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    preds = model.predict(img_array)
     return preds
 
 
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
     return render_template('index.html')
 
 
@@ -48,8 +53,8 @@ def upload():
         f.save(file_path)
 
         # Make prediction
-        #preds = model_predict(file_path, model)
-        #print(preds[0])
+        preds = model_predict(file_path, model)
+        print(preds[0])
 
         # x = x.reshape([64, 64]);
         disease_class = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight',
@@ -57,12 +62,32 @@ def upload():
                          'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
                          'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
                          'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
-        #a = preds[0]
-        #ind=np.argmax(a)
-        #print('Prediction:', disease_class[ind])
-        #result=disease_class[ind]
-        return 'result'
+        a = preds[0]
+        ind=np.argmax(a)
+        print('Prediction:', disease_class[ind])
+        result=disease_class[ind]
+        return result
     return None
+
+
+@app.route('/about')
+def about():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    return render_template('about.html')
+
+@app.route('/recommendations')
+def recommendations():
+    #if 'user' not in session:
+    #    return redirect(url_for('login'))
+    # Example logic (you can expand this based on prediction results)
+    recommended_actions = [
+        "Use fungicide X during early stages.",
+        "Ensure proper crop rotation.",
+        "Improve drainage to reduce humidity."
+    ]
+    return render_template('recommendations.html', recommendations=recommended_actions)
+
 
 
 if __name__ == '__main__':
